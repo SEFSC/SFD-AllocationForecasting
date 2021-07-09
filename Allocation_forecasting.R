572bce4b5dc259802483cef752109f96cd81f949
Nathan Vaughan <nathan.vaughan@noaa.gov>
2021-07-09 09:52:31 -0400

    F_rebuild
    Added capacity to calculate F_rebuild target

--- Allocation_forecasting.R
+++ Allocation_forecasting.R
@@ -1,20 +1,36 @@
-remotes::install_github(repo="r4ss/r4ss",ref="development")
+##---------------------------------------------------------------------##
+#                                                                       #
+#  SS projection package to achieve target equilibrium stock status,    #
+#  constant annual fishing mortality rate, fixed group catch            #
+#  allocations, and historic relative fishing effort between fleets     #
+#  within groups. The package is able to calculate OFL, ABC represented #
+#  as a fraction of the OFL F, and F rebuild represented as a fixed     #
+#  reduction in F during the specified rebuild period if needed.        #
+#                                                                       #
+##---------------------------------------------------------------------##
+#                                                                       #
+#  Author: Nathan Vaughan                                               #
+#  Last update date: 7/9/21                                             #
+#  Contact: nathan.vaughan@noaa.gov                                     #
+#                                                                       #
+##---------------------------------------------------------------------##
 
-library(r4ss)
+#remotes::install_github(repo="r4ss/r4ss",ref="development")
+#library(r4ss)
+#run.projections(assessment_dir="C:/Users/Nathan/Desktop/run25/run25.10", ABC_Fraction=.75, Rebuild_yr=2029)
 
-#Test 
-
-#This is currently set up for yellowtail snapper. Will need work to generalize to all assessment formats.
-
-run.projections(assessment_dir="C:/Users/Nathan/Desktop/run25/run25.10")
-
-run.projections<-function(assessment_dir, #Here you set the location of a previously fit SS stock assessment
-                          PstarFraction = 0.75, #Set the P* target as a fraction of the OFL target
+# This is now a generalized function to run projections that should work for most species. 
+# The function is now designed to mostly read out settings from the forecast file the 
+# same way you would generally set things up for a base SS projection.
+# The only additional inputs needed are a 
+run.projections<-function(assessment_dir, #Here you set the location of a previously fit SS stock assessment to perform projections
+                          ABC_Fraction = NULL, #Set the ABC target as a fraction of the OFL target if NULL will not fit ABC projections
+                          Rebuild_yr = NULL, #Set the rebuild target year if NULL will not fit rebuild projections
                           Depletion.Threshold = 0.0001, # These are all just thresholds for when 
                           Annual.F.Threshold = 0.0001, # targets are acceptably achieved these default to a .01% change
                           Allocation.Threshold = 0.0001, # increase them if run is too slow.
-                          rec_devs = rep(0,100), 
-                          Fcast_impl_error = rep(0,100) #defaults to no rec devs or implementation error  
+                          rec_devs = rep(0,100), #Input for custom rec_devs and below implementation error needs a vector of 100 values one 
+                          Fcast_impl_error = rep(0,100) #for each year of forecast. Defaults to no rec devs or implementation error.  
                           ) 
 {
   #Set large max print to avoid issues with writing out a large forecast file.
@@ -27,7 +43,7 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
   }
   dir.create(paste0(assessment_dir,"/Working_dir"))
   temp.files <- list.files(path=assessment_dir)
-  folders <- c(which(temp.files=="Depletion_target"), which(temp.files=="Pstar_target"), which(temp.files=="Working_dir"))
+  folders <- c(which(temp.files=="OFL_target"), which(temp.files=="ABC_target"), which(temp.files=="Rebuild_target"), which(temp.files=="Working_dir"))
   if(length(folders)>0){
     temp.files <- temp.files[-folders]
   }
@@ -41,8 +57,6 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
   results <- SS_output(dir = getwd(), covar = FALSE, checkcor = FALSE)
   forecast <- SS_readforecast() 
   parlist <- SS_readpar_3.30(parfile = "ss.par", datsource = dat, ctlsource = ctl)
-  
-  
   
   #Modify assessment files to produce expected timeseries length and outputs
   #100 year projections allow equilibrium to be achieved
@@ -117,6 +131,7 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
   forecast_F<-as.data.frame(forecast_F)
   colnames(forecast_F)<-c("Year","Seas","Fleet","Catch or F","Basis")
   
+  
   #Extract fixed forecast values from the forecast file these will be fixed at these values
   if(!is.null(forecast[["ForeCatch"]])){
     Fixed_catch_basis <- forecast[["InputBasis"]]
@@ -135,6 +150,8 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
   }
   
   forecast_F[fixed_ref,c(4,5)]<-Fixed_catch_target[,c(4,5)]
+  
+  rebuild_ref <- which(forecast_F[,1]<=Rebuild_yr)
   
   n_groups <- forecast[["N_allocation_groups"]]
   groups <- rep(0,length(F_cols))
@@ -171,17 +188,15 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
   #Now start a loop of projecting and modifying fixed F's until the desired 
   #catch projections are achieved
   keepFitting <- TRUE
-  fitting_depletion <- TRUE
+  fitting_OFL <- TRUE
+  fitting_ABC <- FALSE
+  fitting_Rebuild <- FALSE
   loop <- 0
   MSY.Fit <- data.frame(catch=c(0),Ave.F=c(0),weight=c(1))
   method <- "MSY/proxy target"
-  par(mfrow=c(3,2))
-  FAdjust <- matrix(c(rep(1,100*length(seasons)*length(F_cols)),
-                      rep(0.1,100*length(seasons)*length(F_cols)),
-                      rep(10,100*length(seasons)*length(F_cols))),
-                    nrow=100*length(seasons)*length(F_cols),
-                    ncol=3, byrow = FALSE)
-  
+  par(mfrow=c(4,2))
+  F_adjust1 <- F_adjust2 <- 1
+  F_adjust3 <- rep(1,100*length(seasons)*length(F_cols))
   while(keepFitting){
     #Read in the SS results for landings and stock status to determine if desired
     #targets have been achieved
@@ -202,14 +217,18 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
     names(TimeFit3)[c(1)] <- c("Yr")
     
     
-    if(fitting_depletion==TRUE){
+    if(fitting_OFL==TRUE){
       
       #Calculate the average F at equilibrium that all F's will be scaled to in order
       #to achieve equal F in every year. As depletion approaches the target value this 
       #F will approach F(OFL).
       F_report<-SPRfit$F_report
       FScale<-mean(F_report[(length(F_report)-9):length(F_report)])
-      Fpstar<-FScale
+      if(!is.null(ABC_Fraction)){
+        F.ABC<-ABC_Fraction*FScale
+      }else{
+        F.ABC<-FScale
+      }
       
       if(Forecast_target==1){
         Target.Depletion <- forecast[["SPRtarget"]]
@@ -241,20 +260,106 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
           f.msy <-  msy.estim$par[3]
         }
         DepletionScale <- f.msy/FScale
+        
+        Depletion <- TimeFit3$SpawnBio/Virgin_bio
+        Target.Rebuild <- mean(Depletion[(length(Depletion)-9):length(Depletion)])
       }else if(Forecast_target==3){
         Target.Depletion <- forecast[["Btarget"]]
         Depletion<-TimeFit3$SpawnBio/Virgin_bio
 
         Achieved.Depletion <- mean(Depletion[(length(Depletion)-9):length(Depletion)])
         DepletionScale <- (1-Target.Depletion)/(1-Achieved.Depletion)
-        #DepletionScale <- Achieved.Depletion/Target.Depletion
-        
         DepletionScale <- (-log(1-((1-exp(-FScale))*DepletionScale))/FScale)
       }
-    }else{
+    }else if(fitting_ABC==TRUE){
       DepletionScale<-1
-      FScale<-PstarFraction*Fpstar
+      FScale<-F.ABC
+    }else if(fitting_Rebuild==TRUE){
+      DepletionScale<-1
+      FScale<-F.ABC
+      F_report<-SPRfit$F_report
+      F_Rebuild_Scale<-F_report[SPRfit$Yr==Rebuild_yr]
+      
+      if(Forecast_target==1){
+        Target.Rebuild <- forecast[["SPRtarget"]]
+        Depletion<-SPRfit$SPR
+        
+        Achieved.Rebuild <- mean(Depletion[SPRfit$Yr==Rebuild_yr])
+        Rebuild.Scale <- (1-Target.Rebuild)/(1-Achieved.Rebuild)
+        Rebuild.Scale <- (-log(1-((1-exp(-F_Rebuild_Scale))*Rebuild.Scale))/F_Rebuild_Scale)
+        Rebuild.Scale <- Rebuild.Scale*F_Rebuild_Scale
+      }else if(Forecast_target==2){
+        Depletion<-TimeFit3$SpawnBio/Virgin_bio
+        
+        Achieved.Rebuild <- mean(Depletion[SPRfit$Yr==Rebuild_yr])
+        Rebuild.Scale <- (1-Target.Rebuild)/(1-Achieved.Rebuild)
+        Rebuild.Scale <- (-log(1-((1-exp(-F_Rebuild_Scale))*Rebuild.Scale))/F_Rebuild_Scale)
+        Rebuild.Scale <- Rebuild.Scale*F_Rebuild_Scale
+      }else if(Forecast_target==3){
+        Target.Rebuild <- forecast[["Btarget"]]
+        Depletion<-TimeFit3$SpawnBio/Virgin_bio
+        
+        Achieved.Rebuild <- mean(Depletion[SPRfit$Yr==Rebuild_yr])
+        Rebuild.Scale <- (1-Target.Rebuild)/(1-Achieved.Rebuild)
+        Rebuild.Scale <- (-log(1-((1-exp(-F_Rebuild_Scale))*Rebuild.Scale))/F_Rebuild_Scale)
+        Rebuild.Scale <- Rebuild.Scale*F_Rebuild_Scale
+      }
+      Rebuild.Scale <- min(Rebuild.Scale,FScale)
     }
+    
+    Fmult2 <- FScale/SPRfit$F_report[sort(rep(seq_along(SPRfit$F_report),length(seasons)*length(F_cols)))]
+    if(fitting_Rebuild==TRUE){
+      Fmult2[rebuild_ref] <- Rebuild.Scale/SPRfit$F_report[sort(rep(seq_along(SPRfit$F_report),length(seasons)*length(F_cols)))][rebuild_ref]
+    }
+    
+    if(loop>1 & fitting_OFL==TRUE){
+      F_adjust1 <- (Last_Mult1-1)/(Last_Mult1-DepletionScale)
+      if(F_adjust1>0){
+        if(DepletionScale<1){
+          DepletionScale <- exp(log(DepletionScale)*F_adjust1)
+        }
+        if(DepletionScale>1){
+          DepletionScale <- ((DepletionScale-1)*F_adjust1+1)
+        }
+      }
+    }else if(loop>1 & fitting_ABC==TRUE){
+      F_adjust2 <- (Last_Mult2-1)/(Last_Mult2-mean(Fmult2[-fixed_ref]))
+      if(F_adjust2 > 0){
+        if(length(Fmult2[Fmult2<1])>0){
+          Fmult2[Fmult2<1] <- exp(log(Fmult2[Fmult2<1])*F_adjust2)
+        }
+        if(length(Fmult2[Fmult2>1])>0){
+          Fmult2[Fmult2>1] <- ((Fmult2[Fmult2>1]-1)*F_adjust2+1)
+        }
+      }
+    }else if(loop>1 & fitting_Rebuild==TRUE){
+      F_adjust2a <- (Last_Mult2a-1)/(Last_Mult2a-mean(Fmult2[rebuild_ref[which(!is.element(rebuild_ref,fixed_ref))]]))
+      F_adjust2b <- (Last_Mult2b-1)/(Last_Mult2b-mean(Fmult2[-sort(unique(c(fixed_ref,rebuild_ref)))]))
+      if(!is.na(F_adjust2a)){
+      if(F_adjust2a > 0){
+        Fmult2a <- Fmult2[rebuild_ref[which(!is.element(rebuild_ref,fixed_ref))]]
+        if(length(Fmult2a[Fmult2a<1])>0){
+          Fmult2a[Fmult2a<1] <- exp(log(Fmult2a[Fmult2a<1])*F_adjust2a)
+        }
+        if(length(Fmult2a[Fmult2a>1])>0){
+          Fmult2a[Fmult2a>1] <- ((Fmult2a[Fmult2a>1]-1)*F_adjust2a+1)
+        }
+        Fmult2[rebuild_ref[which(!is.element(rebuild_ref,fixed_ref))]] <- Fmult2a
+      }}
+      if(!is.na(F_adjust2b)){
+      if(F_adjust2b > 0){
+        Fmult2b <- Fmult2[-sort(unique(c(fixed_ref,rebuild_ref)))]
+        if(length(Fmult2b[Fmult2b<1])>0){
+          Fmult2b[Fmult2b<1] <- exp(log(Fmult2b[Fmult2b<1])*F_adjust2b)
+        }
+        if(length(Fmult2b[Fmult2b>1])>0){
+          Fmult2b[Fmult2b>1] <- ((Fmult2b[Fmult2b>1]-1)*F_adjust2b+1)
+        }
+        Fmult2[-sort(unique(c(fixed_ref,rebuild_ref)))] <- Fmult2b
+      }}
+    }
+    
+    Fmult1 <- rep(DepletionScale,100*length(seasons)*length(F_cols))
     
     if(n_groups>0){
       Catch_temp <- TimeFit3[,Catch_cols3]
@@ -268,26 +373,31 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
       }
     }
     
-    Fmult1 <- rep(DepletionScale,100*length(seasons)*length(F_cols))
-    Fmult2 <- FScale/SPRfit$F_report[sort(rep(seq_along(SPRfit$F_report),length(seasons)*length(F_cols)))]
     Fmult3 <- (0.5*(Allocations[,5]/Allocations[,6]-1)+1)
-    #Fmult3 <- (-log(1-((1-exp(-forecast_F[,4]))*Fmult3))/forecast_F[,4])
+    
     Fmult1[fixed_ref] <- 1
     Fmult2[fixed_ref] <- 1
     Fmult3[fixed_ref] <- 1
     Comb_Mult <- Fmult1*Fmult2*Fmult3
-    Final_Mult <- Comb_Mult
+    
+    Last_Mult1 <- DepletionScale
+    Last_Mult2 <- mean(Fmult2[-fixed_ref])
+    Last_Mult2a <- mean(Fmult2[rebuild_ref[which(!is.element(rebuild_ref,fixed_ref))]])
+    Last_Mult2b <- mean(Fmult2[-sort(unique(c(fixed_ref,rebuild_ref)))])
     
     col_options <- c("black","dark red","dark green","dark blue","orange","purple","red","green","blue","brown","pink","yellow",colors())
     point_options <- c(16,15,17,18,8,9,10,11,12,13,0,1,2,3,4,5,6,14,21,22,23,24,25,19,20)
     plot(Fmult1,xlab="year/season/fleet",ylab="Depletion Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
+    plot(rep(F_adjust1,100*length(seasons)*length(F_cols)),xlab="year/season/fleet",ylab="Depletion Optimization Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
     plot(Fmult2,xlab="year/season/fleet",ylab="F Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
+    plot(rep(F_adjust2,100*length(seasons)*length(F_cols)),xlab="year/season/fleet",ylab="F Optimization Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
     plot(Fmult3,xlab="year/season/fleet",ylab="Allocation Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
-    plot(FAdjust[,1],xlab="year/season/fleet",ylab="Optimization Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
+    plot(F_adjust3,xlab="year/season/fleet",ylab="Allocation Optimization Adjustment",col=rep(col_options[seq_along(F_cols)],100*length(seasons)),pch=rep(sort(rep(point_options[seq_along(seasons)],length(F_cols))),100),main = paste0(method," loop = ",loop))
     
-    if(max(abs(1-Fmult1))>=Depletion.Threshold | max(abs(1-Fmult2))>=Annual.F.Threshold | max(abs(1-Fmult3))>=Allocation.Threshold  | loop<10){keepFitting<-TRUE}else{keepFitting<-FALSE}
+    if(max(abs(1-Fmult1))>=Depletion.Threshold | max(abs(1-Fmult2))>=Annual.F.Threshold | max(abs(1-Fmult3))>=Allocation.Threshold){keepFitting<-TRUE}else{keepFitting<-FALSE}
+    if(fitting_OFL==TRUE & Forecast_target==2 & loop<10){keepFitting<-TRUE}
     
-    forecast_F[,4] <- forecast_F[,4]*Final_Mult
+    forecast_F[,4] <- forecast_F[,4]*Comb_Mult
     forecast_F[fixed_ref,4] <- Fixed_catch_target[,4]
     forecast[["ForeCatch"]] <- forecast_F
     #Write the modified forecast data out to a file and rerun projections
@@ -295,26 +405,53 @@ run.projections<-function(assessment_dir, #Here you set the location of a previo
     shell(paste("cd /d ",getwd()," && ss -nohess",sep=""))
     #If all values have converged check if this is the OFL loop or the P* loop
     if(keepFitting==FALSE){
-      if(fitting_depletion==TRUE){
-        method <- "P*"
-        #If in the OLF loop then reset to keep fitting and change the target from OFL to P*
-        keepFitting <- TRUE
-        fitting_depletion <- FALSE
+      if(fitting_OFL==TRUE){
+        fitting_OFL <- FALSE
+        if(!is.null(ABC_Fraction)){
+          #If in the OLF loop then reset to keep fitting and change the target from OFL to P*
+          loop <- 0
+          method <- "P*"
+          keepFitting <- TRUE
+          fitting_ABC <- TRUE
+        }else if(!is.null(Rebuild_yr)){
+          #If in the OLF loop then reset to keep fitting and change the target from OFL to Rebuild
+          loop <- 0
+          method <- "Rebuild"
+          keepFitting <- TRUE
+          fitting_Rebuild <- TRUE
+        }
         #Write out the OFL results to a new folder (replace any old folder that exists)
-        if(dir.exists(paste0(assessment_dir,"/Depletion_target"))){
-          unlink(paste0(assessment_dir,"/Depletion_target"),recursive = TRUE)
+        if(dir.exists(paste0(assessment_dir,"/OFL_target"))){
+          unlink(paste0(assessment_dir,"/OFL_target"),recursive = TRUE)
         }
-        dir.create(paste0(assessment_dir,"/Depletion_target"))
+        dir.create(paste0(assessment_dir,"/OFL_target"))
         temp.files <- list.files(path=paste0(assessment_dir,"/Working_dir"))
-        file.copy(from=paste0(assessment_dir,'/Working_dir/',temp.files),to=paste0(assessment_dir,"/Depletion_target/",temp.files))
-      }else{
-        #If on the P* loop write out the P* results to a new folder and end the procedure
-        if(dir.exists(paste0(assessment_dir,"/Pstar_target"))){
-          unlink(paste0(assessment_dir,"/Pstar_target"),recursive = TRUE)
+        file.copy(from=paste0(assessment_dir,'/Working_dir/',temp.files),to=paste0(assessment_dir,"/OFL_target/",temp.files))
+      }else if(fitting_ABC==TRUE){
+        fitting_ABC <- FALSE
+        if(!is.null(Rebuild_yr)){
+          #If in the P* loop then reset to keep fitting and change the target from P* to Rebuild
+          loop <- 0
+          method <- "Rebuild"
+          keepFitting <- TRUE
+          fitting_Rebuild <- TRUE
         }
-        dir.create(paste0(assessment_dir,"/Pstar_target"))
+        #If on the P* loop write out the P* results to a new folder (replace any old folder that exists)
+        if(dir.exists(paste0(assessment_dir,"/ABC_target"))){
+          unlink(paste0(assessment_dir,"/ABC_target"),recursive = TRUE)
+        }
+        dir.create(paste0(assessment_dir,"/ABC_target"))
         temp.files <- list.files(path=paste0(assessment_dir,"/Working_dir"))
-        file.copy(from=paste0(assessment_dir,'/Working_dir/',temp.files),to=paste0(assessment_dir,"/Pstar_target/",temp.files))
+        file.copy(from=paste0(assessment_dir,'/Working_dir/',temp.files),to=paste0(assessment_dir,"/ABC_target/",temp.files))
+      }else if(fitting_Rebuild==TRUE){
+        fitting_Rebuild <- FALSE
+        #If on the Rebuild loop write out the Rebuild results to a new folder (replace any old folder that exists) and end the fitting process
+        if(dir.exists(paste0(assessment_dir,"/Rebuild_target"))){
+          unlink(paste0(assessment_dir,"/Rebuild_target"),recursive = TRUE)
+        }
+        dir.create(paste0(assessment_dir,"/Rebuild_target"))
+        temp.files <- list.files(path=paste0(assessment_dir,"/Working_dir"))
+        file.copy(from=paste0(assessment_dir,'/Working_dir/',temp.files),to=paste0(assessment_dir,"/Rebuild_target/",temp.files))
       }
     }
   }
